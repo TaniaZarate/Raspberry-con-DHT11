48d5f49eb2f126a2157079ad551afeffa6d3e8e2
Tania Zárate <90163838+TaniaZarate@users.noreply.github.com>
2022-01-09 18:12:58 -0600

    Add files via upload

---
+++ dht11.py
@@ -0,0 +1,27 @@
+#Codigo recuperado de: https://www.freva.com/dht11-temperature-and-humidity-sensor-on-raspberry-pi/
+
+import time
+import board
+import adafruit_dht
+import psutil
+
+for proc in psutil.process_iter():
+    if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
+        proc.kill()
+
+sensor = adafruit_dht.DHT11(board.D23)
+
+while True:
+    try:
+        temp = sensor.temperature
+        humidity = sensor.humidity
+        print("Temperatura: {}*C   Humedad: {}% ".format(temp, humidity))
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        sensor.exit()
+        raise error
+
+    time.sleep(2.0)
